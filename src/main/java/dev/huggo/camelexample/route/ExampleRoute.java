3c972ec814a7b8682a12c4d8ce767e8090fffbb9
devhuggo <h.parcelly@gmail.com>
2026-01-07 20:28:34 -0300

    finish the first example

--- src/main/java/dev/huggo/camelexample/route/ExampleRoute.java
+++ src/main/java/dev/huggo/camelexample/route/ExampleRoute.java
@@ -5,7 +5,8 @@ import org.apache.camel.builder.RouteBuilder;
 public class ExampleRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("file:src/main/resources/files")
+        from("file:src/main/resources/files?noop=true")
+                .log("Message: ${body}, Headers: ${headers}")
                 .to("file:src/main/resources/files/output");
     }
 }
