13df863fb6ed029bf6bb41600ed5c44f24f258e1
devhuggo <h.parcelly@gmail.com>
2026-01-20 22:28:50 -0300

    add error handling using on exception

--- src/main/java/dev/huggo/camelexample/Main.java
+++ src/main/java/dev/huggo/camelexample/Main.java
@@ -2,6 +2,7 @@ package dev.huggo.camelexample;
 
 
 import dev.huggo.camelexample.route.ErrorHandlerExampleRoute;
+import dev.huggo.camelexample.route.OnExceptionExampleRoute;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -10,7 +11,8 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class Main {
     public static void main(String[] args) {
         try (CamelContext camelContext = new DefaultCamelContext()) {
-            camelContext.addRoutes(new ErrorHandlerExampleRoute());
+//            camelContext.addRoutes(new ErrorHandlerExampleRoute());
+            camelContext.addRoutes(new OnExceptionExampleRoute());
 
             camelContext.start();
             Thread.sleep(2000);
