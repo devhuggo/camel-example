e458f1931518a5b03e198cdf6e6251427e01b8aa
devhuggo <h.parcelly@gmail.com>
2026-01-20 22:15:35 -0300

    add error handler using deadLetterChannel

--- src/main/java/dev/huggo/camelexample/route/ErrorHandlerExampleRoute.java
+++ src/main/java/dev/huggo/camelexample/route/ErrorHandlerExampleRoute.java
@@ -6,7 +6,9 @@ public class ErrorHandlerExampleRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("timer:ticker?period=1s")
-                .errorHandler(defaultErrorHandler().maximumRedeliveries(3).redeliveryDelay(500))
+                .errorHandler(
+                        deadLetterChannel("file:src/main/resources/files/error?fileName=example-error.txt")
+                            .maximumRedeliveries(3))
                 .setBody(simple("Hello World"))
                 .process(exchange -> {
                     throw new RuntimeException("Example exception");
